7cad7f3b9911b2b6f58203e10b3f69b7bf2eb3cb
Nabin Sah <nabinsah703@gmail.comgit config --global user.email nabinsah703@gmail.com>
2022-03-30 17:01:34 +0545

    hotel menu card

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+menu1 = SecretMenuItem.create(menu_name: "Chipotle Nachos", restaurant_name: "KFC", menu_description: "build a plaate of nachos with all of your favorite fixing ")
+menu2 = SecretMenuItem.create(menu_name: "Starbucks butterbeer Frappuccino", restaurant_name: "Starbucks", menu_description:"Combine three pumps of toffee nut syrup and three pumps of caramel with a Crème Frappuccino base")
+menu3 = SecretMenuItem.create(menu_name: "Skittles", restaurant_name: "Jamba Juice", menu_description:"A mixture of lemonade, lime sherbet, froz")
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
