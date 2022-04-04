431895b240782178d6f8408325b5aef5ce3c4d6b
Nabin Sah <nabinsah703@gmail.comgit config --global user.email nabinsah703@gmail.com>
2022-04-04 23:14:35 +0545

    change in code

--- app/controllers/secret_menu_items_controller.rb
+++ app/controllers/secret_menu_items_controller.rb
@@ -3,18 +3,32 @@ class SecretMenuItemsController < ApplicationController
         @secretMenuItems = SecretMenuItem.all
         render json: @SecretMenuItems
     end
+
     def show
         @secretMenuItem = SecretMenuItem.find(params[:id])
         render json: @secretMenuItem
     end
+    
     def create
-        @secretMenuItem = SecretMenuItem.create(
-            menu_name: params[:menu_name],
-            restaurant_name: params[:restaurant_name],
-            menu_description: params[:menu_description]
+        
+        @secret_menu_item = SecretMenuItem.new(
+            secret_menu_item_params  
         )
-        render json: @secretMenuItems
+
+       if @secret_menu_item.valid?
+            @secret_menu_item.save
+            render json: @secret_menu_item
+           
+       else 
+        render json: {
+            status: 422,
+            error: "Invalid Body"
+        }
+           
+       end
+        
     end
+    
     def update
         @secretMenuItem = SecretMenuItem.find(params[:id])
         @secretMenuItem.update(
@@ -32,6 +46,11 @@ class SecretMenuItemsController < ApplicationController
         render json: @secretMenuItem
     end
     
+    private
+
+    def secret_menu_item_params
+      params.require(:secret_menu_item).permit(:menu_name, :restaurant_name, :menu_description)
+    end
 
 
 end
